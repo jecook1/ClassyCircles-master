7883f0c3755fee629639ab07fc410027fb1e452b
James Cook <jcook8@student.strode-college.ac.uk>
2017-09-19 15:54:46 +0100

    Add project files.

---
+++ ClassyCircles/CCircle.vb
@@ -0,0 +1,34 @@
+﻿Public Class CCircle
+
+    ' This is called CCircle the extra C indicates that it is a Class definition
+
+    ' It contains the properties (variables) and methods (subroutines)
+    ' that are needed in each Circle
+
+    Private XPosition As Integer
+    Private YPosition As Integer
+    Private Diameter As Integer
+
+    Public Sub New(X As Integer, Y As Integer, D As Integer)
+
+        XPosition = X
+        YPosition = Y
+        Diameter = D
+
+    End Sub
+
+    Public Sub Draw(g As Graphics)
+
+        Dim Pen As New Pen(Color.Blue, 2)
+
+        g.DrawEllipse(Pen, XPosition, YPosition, Diameter, Diameter)
+
+    End Sub
+
+    Public Sub Drop()
+
+        YPosition += Diameter \ 10
+
+    End Sub
+
+End Class
---
+++ ClassyCircles/MainForm.vb
@@ -0,0 +1,34 @@
+﻿Public Class MainForm
+
+    Private Circles(2) As CCircle
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Circles(0) = New CCircle(50, 150, 10)
+        Circles(1) = New CCircle(250, 50, 30)
+        Circles(2) = New CCircle(150, 50, 50)
+
+    End Sub
+
+    Private Sub Display_Paint(sender As Object, e As PaintEventArgs) Handles Display.Paint
+
+        Dim g As Graphics
+
+        g = e.Graphics
+
+        For Each Circle In Circles
+            Circle.Draw(g)
+        Next
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        For Each Circle In Circles
+            Circle.Drop()
+        Next
+
+        Display.Refresh()
+
+    End Sub
+End Class
